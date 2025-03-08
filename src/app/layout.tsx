f20ca61061e27dfdf949bea54cf5d8ef03fa7de9
Nicolas Torres <ntorres.dev@gmail.com>
2025-03-08 02:14:24 +0100

    adds analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import { siteConfig } from "@/lib/config";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -24,6 +25,7 @@ export default function RootLayout({
           {/* Footer is shown on all pages except auth pages */}
         </TRPCReactProvider>
       </body>
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-H6YND53W1B" />
     </html>
   );
 }
